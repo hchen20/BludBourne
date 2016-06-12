d8fef1a1d81ca4f40597167fbe354449b6013bad
Hang Chen <hchen1991@gmail.com>
2016-06-12 12:30:13 -0400

    Component interfaces implemented. isCollisionWithMapEntities and isCollision method are implemented in PhysicsComponent class

--- core/src/com/mygdx/libgdx/Component.java
+++ core/src/com/mygdx/libgdx/Component.java
@@ -3,5 +3,24 @@ package com.mygdx.libgdx;
 /**
  * Created by Hang Chen on 6/12/2016.
  */
-public class Component {
+public interface Component {
+    public static final String MESSAGE_TOKEN = ":::::";
+
+    public static enum MESSAGE {
+        CURRENT_POSITION,
+        INIT_START_POSITION,
+        CURRENT_DIRECTION,
+        CURRENT_STATE,
+        COLLISION_WITH_MAP,
+        COLLISION_WITH_ENTITY,
+        LOAD_ANIMATIONS,
+        INIT_DIRECTIONS,
+        INIT_SELECT_ENTITY,
+        INIT_STATE,
+        ENTITY_SELECTED,
+        ENTITY_DESELECTED
+    }
+
+    void dispose();
+    void receiveMessage(String message);
 }
--- core/src/com/mygdx/libgdx/PhysicsComponent.java
+++ core/src/com/mygdx/libgdx/PhysicsComponent.java
@@ -1,7 +1,59 @@
 package com.mygdx.libgdx;
 
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.utils.Array;
+
 /**
  * Created by Hang Chen on 6/12/2016.
  */
-public class PhysicsComponent {
+public abstract class PhysicsComponent implements Component {
+    public abstract void update(Entity entity, MapManager mpaMgr, float delta);
+
+    public Rectangle _boundingBox;
+    protected BoundingBoxLocation _boundingBoxLocation;
+
+    public static enum BoundingBoxLocation {
+        BOTTOM_LEFT,
+        BOTTOM_CENTER,
+        CENTER,
+    }
+
+    PhysicsComponent() {
+
+    }
+
+    protected boolean isCollisionWithMapEntities(Entity entity, MapManager mapMgr) {
+        Array<Entity> entities = mapMgr.getCurrentEntities();
+        boolean isCollisionWithMapEntities = false;
+
+        for (Entity mapEntity : entities) {
+            // Check for testing against itself
+            if (mapEntity.equals(entity))
+                continue;
+
+            Rectangle targetRec = mapEntity.getCurrentBoundingBox();
+            if (_boundingBox.overlaps(targetRec)) {
+                // Collision
+                entity.sendMessage(MESSAGE.COLLISION_WITH_ENTITY);
+                isCollisionWithMapEntities = true;
+                break;
+            }
+        }
+
+        return isCollisionWithMapEntities;
+    }
+
+    protected boolean isCollision(Entity source, Entity target) {
+        boolean isCollision = false;
+
+        if (source.equals(target))
+            return false;
+
+        if (source.getCurrentBoundingBox().overlaps(target.getCurrentBoundingBox())) {
+            source.sendMessage(MESSAGE.COLLISION_WITH_ENTITY);
+            isCollision = true;
+        }
+
+        return isCollision;
+    }
 }
