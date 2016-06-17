94008f66be0d260046f7a2458a295822f8de53ff
Hang Chen <hchen1991@gmail.com>
2016-06-17 02:17:45 -0400

    Partially implemented TownMap class Declarations only

--- core/src/com/mygdx/libgdx/TownMap.java
+++ core/src/com/mygdx/libgdx/TownMap.java
@@ -3,5 +3,13 @@ package com.mygdx.libgdx;
 /**
  * Created by Hang Chen on 6/17/2016.
  */
-public class TownMap {
+public class TownMap extends Map {
+    private static final String TAG = PlayerPhysicsComponent.class.getSimpleName();
+
+    private static String _mapPath = "maps/town.tmx";
+    private static String _townGuardWalking = "scripts/town_guard_walking.json";
+    private static String _townBlacksmith = "scripts/town_blacksmith.json";
+    private static String _townMage = "scripts/town_mage.json";
+    private static String _townInnKeeper = "scripts/town_innkeeper.json";
+    private static String _townFolk = "scripts/town_folk.json";
 }
